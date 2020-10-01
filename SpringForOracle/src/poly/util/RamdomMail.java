9e600cd40c66093b812d3a2edac80252df556022
me940728 <me940728@naver.com>
2020-10-01 10:51:07 +0900

    랜덤값 생성 메서드 추가

--- SpringForOracle/src/poly/util/RamdomMail.java
+++ SpringForOracle/src/poly/util/RamdomMail.java
@@ -5,10 +5,11 @@ import java.util.Random;
 public class RamdomMail {
 	
 	public static StringBuffer SendRamdomMail() {
-		
+		// 랜덤 생성 메서드
 		Random rand = new Random();
+		// 랜덤 값 담을 변수 
 		StringBuffer strBu = new StringBuffer();
-		
+		// 반복문으로 6자리 생성 대뮨자, 소문자, 숫자 케이스 3개
 		for (int i = 0; i < 6; i++) {
 			int index = rand.nextInt(3);
 			switch (index) {
@@ -23,6 +24,7 @@ public class RamdomMail {
 				break;
 			}
 		}
+		// 값 담아 리턴  -> 차후 메일 발송 및 패스워드 변결 시 사용 예정
 		return strBu;
 	}
 }
