0de9d8db2cbce61b4ccdbd36316ddc9d08d78c4f
me940728 <me940728@naver.com>
2020-11-09 14:43:02 +0900

    1D1C

--- SpringForOracle/src/poly/controller/UserController.java
+++ SpringForOracle/src/poly/controller/UserController.java
@@ -102,25 +102,6 @@ public class UserController {
 		return "/user/redirect";
 	}
 
-	// 로그아웃 처리 매퍼
-	@RequestMapping(value = "user/userLogOut.do")
-	public String logOut(HttpSession session, Model model) throws Exception {
-		log.info(this.getClass() + "user/logOut start!!");
-
-		String msg = "";
-		String url = "";
-		msg = "로그아웃 하시겠습니까?";
-		url = "/user/userLogin.do";
-
-		session.invalidate(); // 세션 정보 초기화
-
-		model.addAttribute("msg", msg);
-		model.addAttribute("url", url);
-
-		log.info(this.getClass() + "user/loginOut end!!");
-
-		return "user/userLogout";
-	}
 
 	// 로그인 성공 후 메인접속 메퍼
 	@RequestMapping(value = "user/mainPage.do")
